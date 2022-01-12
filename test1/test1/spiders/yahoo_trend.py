f165643c709546d10828dd94943bd19d7785b05d
Liberbell <liberbell@gmail.com>
2022-01-12 15:21:00 +0900

    yahootrend2

--- test1/test1/spiders/yahoo_trend.py
+++ test1/test1/spiders/yahoo_trend.py
@@ -4,7 +4,7 @@ import scrapy
 class YahooTrendSpider(scrapy.Spider):
     name = 'yahoo_trend'
     allowed_domains = ['www.yahoo.co.jp']
-    start_urls = ['http://www.yahoo.co.jp/']
+    start_urls = ['https://www.yahoo.co.jp/']
 
     def parse(self, response):
         pass
