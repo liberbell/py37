8904fc3e1c9448751f398d1f6f3148e9d3214e90
Liberbell <liberbell@gmail.com>
2022-01-12 22:48:57 +0900

    newsscrape1

--- newspaper.py
+++
@@ -1,5 +0,0 @@
-import newspaper
-from newspaper import article
-
-URL = "https://news.yahoo.co.jp/pickup/6415002"
-article = article(URL)
---
+++ newsscrape.py
@@ -0,0 +1,4 @@
+# import newspaper
+from newspaper import Article
+
+URL = "https://news.yahoo.co.jp/pickup/6415002"
