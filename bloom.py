a508c431f8e4a02384c09ab194439227f3378792
Liberbell <liberbell@gmail.com>
2022-01-13 08:48:15 +0900

    bloom2

--- bloom.py
+++ bloom.py
@@ -3,6 +3,7 @@ import newspaper
 URL = "https://www.bloomberg.co.jp/"
 response = newspaper.build(URL, memoize_articles = False)
 
+count = 0
 for article in response.articles:
     article.download()
     article.parse()
@@ -10,3 +11,7 @@ for article in response.articles:
     print(article.title)
     print(article.url)
     print(article.summary, end="\n\n")
+
+    if i > 9:
+        break
+    i = i + 1
