bc3bf80d808d09b3e31d6714428d3991fba8099e
Liberbell <liberbell@gmail.com>
2022-01-14 08:46:20 +0900

    itmedia5

--- itmedia.py
+++ itmedia.py
@@ -1,17 +1,19 @@
 import newspaper
 
-URL = "https://www.itmedia.co.jp/"
-webdata = newspaper.build(URL, memoize_articles = False)
+# URL = "https://www.itmedia.co.jp/"
+URL = ["https://www.itmedia.co.jp/", "https://thebridge.jp/"]
+# webdata = newspaper.build(URL, memoize_articles = False)
 
-i = 1
-for article in webdata.articles:
-    article.download()
-    article.parse()
-    article.nlp()
-    print("Content:", str(i), ":", article.title)
-    print(article.url)
-    print(article.summary, end="\n\n")
+# i = 1
+# for article in webdata.articles:
+#     article.download()
+#     article.parse()
+#     article.nlp()
+#     print("Content:", str(i), ":", article.title)
+#     print(article.url)
+#     print(article.summary, end="\n\n")
 
-    if i > 9:
-        break
-    i = i + 1
+#     if i > 9:
+#         break
+#     i = i + 1
+
