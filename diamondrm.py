f3766afa1910707f2ec4404f5e056bebabdc048f
Liberbell <liberbell@gmail.com>
2022-01-13 09:22:32 +0900

    diamondrm3

--- diamondrm.py
+++ diamondrm.py
@@ -1,3 +1,7 @@
 from newspaper import Article
 
 URL = "https://diamond-rm.net/management/102956/"
+article = Article(URL, memoize_articles = False)
+article.download()
+article.parse()
+article.publish.date()
