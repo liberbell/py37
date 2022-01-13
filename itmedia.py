5375aa6e376b37c86b75b05e68164e3e4cb421fd
Liberbell <liberbell@gmail.com>
2022-01-14 08:37:24 +0900

    itmedia1

--- itmedia.py
+++ itmedia.py
@@ -1,8 +1,10 @@
 import newspaper
 
 URL = "https://www.itmedia.co.jp/"
-article = newspaper.build(URL, momoize_articles=False)
+webdata = newspaper.build(URL, momoize_articles=False)
 
 i = 1
-for i in article.articles:
+for article in webdata.articles:
     article.download()
+    article.perse()
+    article.nlp()
