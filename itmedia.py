5dbe11181ef4a38f9352622c2c43450ad985dfa0
Liberbell <liberbell@gmail.com>
2022-01-14 08:39:26 +0900

    itmedia2

--- itmedia.py
+++ itmedia.py
@@ -8,3 +8,10 @@ for article in webdata.articles:
     article.download()
     article.perse()
     article.nlp()
+    print("Content:", str(i), ":", article.title)
+    print(article.url)
+    print(article.summary, end="\n\n")
+
+    if i > 9:
+        break
+    i = i + 1
