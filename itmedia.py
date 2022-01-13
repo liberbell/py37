8250e2c9438bae181202e45b4f91d7699c9e7898
Liberbell <liberbell@gmail.com>
2022-01-14 08:49:54 +0900

    itmedia7

--- itmedia.py
+++ itmedia.py
@@ -18,4 +18,16 @@ URLs = ["https://www.itmedia.co.jp/", "https://thebridge.jp/"]
 #     i = i + 1
 
 for url in URLs:
-    webdata = newspaper.build(URL, memoize_articles = False, language="ja")
+    webdata = newspaper.build(url, memoize_articles = False, language="ja")
+    i = 1
+    for article in webdata.articles:
+        article.download()
+        article.parse()
+        article.nlp()
+        print("Content:", str(i), ":", article.title)
+        print(article.url)
+        print(article.summary, end="\n\n")
+
+        if i > 9:
+            break
+        i = i + 1
