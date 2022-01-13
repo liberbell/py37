1608691f27372c2138f3a32f9416388889bed654
Liberbell <liberbell@gmail.com>
2022-01-13 09:23:08 +0900

    diamondrm4

--- diamondrm.py
+++ diamondrm.py
@@ -4,4 +4,4 @@ URL = "https://diamond-rm.net/management/102956/"
 article = Article(URL, memoize_articles = False)
 article.download()
 article.parse()
-article.publish.date()
+print(article.publish_date)
