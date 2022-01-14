81f58d65fbdfb1529b26eeec1a1d7f13bbdfdfd0
Liberbell <liberbell@gmail.com>
2022-01-14 13:41:58 +0900

    pandascrape7

--- pandascrape.py
+++ pandascrape.py
@@ -5,5 +5,7 @@ URL = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL"
 # webdata = pd.read_html(URL, header=0)
 webdata = pd.read_html(requests.get(URL, headers={'User-agent': 'Mozilla/5.0'}).text,header=0)
 
-print(webdata[0].head())
+# print(webdata[0].head())
 print(webdata[0].tail())
+
+webdata[0]["Adj Close**"] = pd.to_numeric(webdata[0]["Adj Close**"], errors="coerce")
