21b71b6500b92220eb0442f582570eef718e2b5d
Liberbell <liberbell@gmail.com>
2022-01-14 09:17:38 +0900

    pandascrape5

--- pandascrape.py
+++ pandascrape.py
@@ -6,3 +6,4 @@ URL = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL"
 webdata = pd.read_html(requests.get(URL, headers={'User-agent': 'Mozilla/5.0'}).text,header=0)
 
 print(webdata[0].head())
+print(webdata[0].tail())
