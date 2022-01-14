490c260607b70d42abdd10df52f51f8c3f8a9ec8
Liberbell <liberbell@gmail.com>
2022-01-14 09:06:35 +0900

    pandascrape3

--- pandascrape.py
+++ pandascrape.py
@@ -1,3 +1,8 @@
 import pandas as pd
+import requests
 
 URL = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL"
+# webdata = pd.read_html(URL, header=0)
+webdata = pd.read_html(requests.get(URL, headers={'User-agent': 'Mozilla/5.0'}).text,header=0)
+
+print(webdata[0])
