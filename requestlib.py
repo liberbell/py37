1d8ff00dcaf9bc4d02b8bba80672a89522329c2c
Liberbell <liberbell@gmail.com>
2022-01-15 14:08:32 +0900

    requestlib8

--- requestlib.py
+++ requestlib.py
@@ -12,4 +12,7 @@ for key, value in response.headers.items():
     print(key, "  ", value)
 
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
-header = ["user_agent": user_agent]
+header = {"user_agent": user_agent}
+
+response = requests.get(URL, headers= header)
+print(response.status_code)
