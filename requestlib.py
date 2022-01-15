cca9638687d8c8ec472170f017a3a34f6ef8672a
Liberbell <liberbell@gmail.com>
2022-01-15 14:06:19 +0900

    requests7

--- requestlib.py
+++ requestlib.py
@@ -10,3 +10,6 @@ print(response.encoding)
 
 for key, value in response.headers.items():
     print(key, "  ", value)
+
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
+header = ["user_agent": user_agent]
