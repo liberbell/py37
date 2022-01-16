4fd077d7d638d4bcd661795165d386cd80e85731
Liberbell <liberbell@gmail.com>
2022-01-16 13:38:47 +0900

    getyomi5

--- getyomi.py
+++ getyomi.py
@@ -7,9 +7,12 @@ webdata = requests.get(URL)
 if webdata.status_code != 200:
     print("Something wrong.")
 
-print(webdata.text)
+# print(webdata.text)
 
 soup = BeautifulSoup(webdata.text, "html.parser")
-print(soup)
+# print(soup)
 
 element = soup.select("div.headline > article:nth-child(1) > div > h3 > a")
+print(element[0])
+print(element[0].contents)
+print(element[0].attrs["href"])
