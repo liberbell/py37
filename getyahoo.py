7403c6129ec77b2f55646d5df47f809d3d360642
Liberbell <liberbell@gmail.com>
2022-01-17 09:14:49 +0900

    getyahoo5

--- getyahoo.py
+++ getyahoo.py
@@ -9,5 +9,6 @@ soup = BeautifulSoup(webdata.text, "html.parser")
 #tabpanelTopics1 > div > div._2jjSS8r_I9Zd6O9NFJtDN- > ul > li:nth-child(1) > article > a > div > div > h1 > span
 #tabpanelTopics1 > div > div._2jjSS8r_I9Zd6O9NFJtDN- > ul > li:nth-child(2) > article > a > div > div > h1 > span
 
-news_list = soup.select("tabpanelTopics1 > div > div._2jjSS8r_I9Zd6O9NFJtDN- > ul > li:nth-child(1) > article > a > div > div > h1")
+# news_list = soup.select("tabpanelTopics1 > div > div._2jjSS8r_I9Zd6O9NFJtDN- > ul > li:nth-child(1) > article > a > div > div > h1")
+news_list = soup.find_all("tabpanelTopics1 > div > div._2jjSS8r_I9Zd6O9NFJtDN- > ul > li:nth-child(1) > article > a > div > div > h1")
 print(news_list[0])
