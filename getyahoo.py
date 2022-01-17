e511a805158ce5114cf1eb846abb9d68c4bda29a
Liberbell <liberbell@gmail.com>
2022-01-17 09:07:19 +0900

    getyahoo2

--- getyahoo.py
+++ getyahoo.py
@@ -1,4 +1,9 @@
 import imp
 from bs4 import BeautifulSoup
 import requests
+import time
 
+URL = "https://www.yahoo.co.jp"
+webdata = requests.get(URL)
+
+#tabpanelTopics1 > div > div._2jjSS8r_I9Zd6O9NFJtDN- > ul > li:nth-child(1) > article > a > div > div > h1 > span
