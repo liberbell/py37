e0671a9ad2ce7d411dd46e1063d04f82fcbc078a
Liberbell <liberbell@gmail.com>
2022-01-19 08:44:44 +0900

    seleniums1

--- selenium1.py
+++ selenium1.py
@@ -2,5 +2,9 @@ from selenium import webdriver
 from time import sleep
 
 URL = "https://www.google.com"
+Search_words = "python"
 driver = webdriver.Chrome("./chromedriver")
 driver.get(URL)
+
+search_bar = driver.find_element_by_name("q")
+search_bar.send_keys(Search_words)
