4363aa36f89064a9bb29353d5b17de21c9fc20ca
Liberbell <liberbell@gmail.com>
2022-01-19 08:52:42 +0900

    selenium3

--- selenium1.py
+++ selenium1.py
@@ -9,3 +9,8 @@ driver.get(URL)
 search_bar = driver.find_element_by_name("q")
 search_bar.send_keys(Search_words)
 search_bar.submit()
+
+for element_h3 in  driver.find_elements_by_xpath("//a/h3"):
+    print(element_h3.text)
+    element_a = element_h3.find_element_by_xpath("..")
+    print(element_a.text)
