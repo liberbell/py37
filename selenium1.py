8de7d6e2b7938c20f229a792bfe40da2f83ee07a
Liberbell <liberbell@gmail.com>
2022-01-19 09:23:35 +0900

    selenium12

--- selenium1.py
+++ selenium1.py
@@ -6,7 +6,8 @@ import datetime
 
 URL = "https://www.google.com"
 Search_words = "python"
-csv_date = datetime.datetime().today().strftime("%Y%m%d")
+csv_date = datetime.datetime.today().strftime("%Y%m%d")
+print(csv_date)
 
 options = Options()
 options.add_argument('--headless')
@@ -18,19 +19,19 @@ search_bar = driver.find_element_by_name("q")
 search_bar.send_keys(Search_words)
 search_bar.submit()
 
-count = 0
-while True:
-    count = count + 1
-    sleep(1)
-    for element_h3 in  driver.find_elements_by_xpath("//a/h3"):
-        print(element_h3.text)
-        element_a = element_h3.find_element_by_xpath("..")
-        print(element_a.get_attribute("href"))
+# count = 0
+# while True:
+#     count = count + 1
+#     sleep(1)
+#     for element_h3 in  driver.find_elements_by_xpath("//a/h3"):
+#         print(element_h3.text)
+#         element_a = element_h3.find_element_by_xpath("..")
+#         print(element_a.get_attribute("href"))
 
-    next_link = driver.find_element_by_id("pnnext")
-    # print(next_link)
-    driver.get(next_link.get_attribute("href"))
+#     next_link = driver.find_element_by_id("pnnext")
+#     # print(next_link)
+#     driver.get(next_link.get_attribute("href"))
 
-    if count > 4:
-        break
+#     if count > 4:
+#         break
 
