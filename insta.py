0e3c77ba7d50de98f6d9a1d105c84fa215ce1197
Liberbell <liberbell@gmail.com>
2022-01-20 15:24:30 +0900

    insta10

--- insta.py
+++ insta.py
@@ -23,14 +23,15 @@ error_flag = False
 
 
 # <input aria-label="Phone number, username, or email" aria-required="true" autocapitalize="off" autocorrect="off" maxlength="75" name="username" type="text" class="_2hvTZ pexuQ zyHYP" value="">
+# <input aria-label="電話番号、ユーザーネーム、メールアドレス" aria-required="true" autocapitalize="off" autocorrect="off" maxlength="75" name="username" type="text" class="_2hvTZ pexuQ zyHYP" value="">
 # <input aria-label="Password" aria-required="true" autocapitalize="off" autocorrect="off" name="password" type="password" class="_2hvTZ pexuQ zyHYP" value="">
 try:
     print()
-    username_input = driver.find_element_by_xpath("//input[@aria-label='Phone number, username, or email']")
+    username_input = driver.find_element_by_xpath("//input[@aria-label='電話番号、ユーザーネーム、メールアドレス']")
     username_input.send_keys(ID)
     sleep(1)
 
-    password_input = driver.find_element_by_xpath("//input[@aria-label='Password']")
+    password_input = driver.find_element_by_xpath("//input[@aria-label='パスワード']")
     password_input.send_keys(PASSWORD)
     sleep(1)
 
