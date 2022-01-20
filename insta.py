1f8251e4f96e89d24e53908d37580e84bf9e8fad
Liberbell <liberbell@gmail.com>
2022-01-20 09:22:49 +0900

    insta4

--- insta.py
+++ insta.py
@@ -1,6 +1,9 @@
 import json
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from time import sleep
+
+URL = "https://www.instagram.com/"
 
 with open("insta_secret.json") as f:
     insta_key = json.load(f)
@@ -8,3 +11,7 @@ ID = insta_key["id"]
 PASSWORD = insta_key["password"]
 
 # print(ID, PASSWORD)
+options = Options()
+options.add_argument('--headless')
+# driver = webdriver.Chrome("./chromedriver", options=options)
+driver = webdriver.Chrome("./chromedriver")
